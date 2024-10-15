ab0df9b8b8722452ce32294d2481a389ff34399f
Höhl <MHOEHL@controlware.de>
2024-10-15 15:48:11 +0200

    initial commit

---
+++ data.tf
@@ -0,0 +1,3 @@
+data "azurerm_resource_group" "hub_rg" {
+  name = "rg-hubnetwork-demo-weu"
+}
---
+++ network.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "hub_vnet" {
+  name                = "vnet-secureaks-demo-weu"
+  location            = data.azurerm_resource_group.hub_rg.location
+  resource_group_name = data.azurerm_resource_group.hub_rg.name
+  address_space       = ["10.0.0.0/23"]
+}
+
+resource "azurerm_subnet" "jumphost_snet" {
+  name                 = "JumphostSubnet"
+  resource_group_name  = data.azurerm_resource_group.hub_rg.name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = ["10.1.0.0/27"]
+}
