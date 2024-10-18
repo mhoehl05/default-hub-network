85e388ea227dd376a40c6e39d980c4372a8681fc
Höhl <MHOEHL@controlware.de>
2024-10-18 11:01:19 +0200

    changed vnet address space and renamed vnet

--- network.tf
+++ network.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "hub_vnet" {
-  name                = "vnet-secureaks-demo-weu"
+  name                = "vnet-hub-demo-weu"
   location            = data.azurerm_resource_group.hub_rg.location
   resource_group_name = data.azurerm_resource_group.hub_rg.name
-  address_space       = ["10.0.0.0/23"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "jumphost_snet" {
   name                 = "JumphostSubnet"
   resource_group_name  = data.azurerm_resource_group.hub_rg.name
   virtual_network_name = azurerm_virtual_network.hub_vnet.name
-  address_prefixes     = ["10.0.0.0/27"]
+  address_prefixes     = ["10.0.0.0/23"]
 }
