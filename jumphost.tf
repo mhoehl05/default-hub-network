1b85e70999c9b5ac29f1054085d6cf553d2e92f6
Höhl <MHOEHL@controlware.de>
2024-11-14 12:07:14 +0100

    only specify ssh keys

--- jumphost.tf
+++ jumphost.tf
@@ -3,10 +3,7 @@ module "jumphost" {
 
   rg_name           = data.azurerm_resource_group.hub_rg.name
   snet_id           = azurerm_subnet.jumphost_snet.id
-  admin_1           = "mark"
   public_key_path_1 = "${path.module}/ssh_keys/id_rsa_mark.pub"
-  admin_2           = "reffat"
   public_key_path_2 = "${path.module}/ssh_keys/id_rsa_reffat.pub"
-  admin_3           = "fabian"
   public_key_path_3 = "${path.module}/ssh_keys/id_rsa_mark.pub"
 }
